ba1cdbf2d4e287ce3145a300aa328684e776099c
Danielle Mayne <danielle.mayne@enstratus.com>
2014-02-28 12:28:00 +0000

    add vm capabilities

---
+++ src/main/java/org/dasein/cloud/virtustream/compute/VMCapabilities.java
@@ -0,0 +1,170 @@
+package org.dasein.cloud.virtustream.compute;
+
+import org.dasein.cloud.AbstractCapabilities;
+import org.dasein.cloud.Capabilities;
+import org.dasein.cloud.CloudException;
+import org.dasein.cloud.InternalException;
+import org.dasein.cloud.Requirement;
+import org.dasein.cloud.compute.Architecture;
+import org.dasein.cloud.compute.ImageClass;
+import org.dasein.cloud.compute.Platform;
+import org.dasein.cloud.compute.VMScalingCapabilities;
+import org.dasein.cloud.compute.VirtualMachineCapabilities;
+import org.dasein.cloud.compute.VmState;
+import org.dasein.cloud.virtustream.Virtustream;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Locale;
+
+/**
+  * Describes the capabilities of Virtustream with respect to Dasein virtual machine operations.
+  * <p>Created by Danielle Mayne: 2/28/14 11:59 AM</p>
+  * @author Danielle Mayne
+  * @version 2014.03 initial version
+  * @since 2014.03
+  */
+
+public class VMCapabilities extends AbstractCapabilities<Virtustream> implements VirtualMachineCapabilities {
+
+
+    public VMCapabilities(@Nonnull Virtustream cloud) { super(cloud); }
+
+    @Override
+    public boolean canAlter(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return fromState.equals(VmState.STOPPED);
+    }
+
+    @Override
+    public boolean canClone(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return true;
+    }
+
+    @Override
+    public boolean canPause(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return false;
+    }
+
+    @Override
+    public boolean canReboot(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return true;
+    }
+
+    @Override
+    public boolean canResume(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return fromState.equals(VmState.RUNNING);
+    }
+
+    @Override
+    public boolean canStart(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return !fromState.equals(VmState.RUNNING);
+    }
+
+    @Override
+    public boolean canStop(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return !fromState.equals(VmState.STOPPED);
+    }
+
+    @Override
+    public boolean canSuspend(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return !fromState.equals(VmState.STOPPED);
+    }
+
+    @Override
+    public boolean canTerminate(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return !fromState.equals(VmState.TERMINATED);
+    }
+
+    @Override
+    public boolean canUnpause(@Nonnull VmState fromState) throws CloudException, InternalException {
+        return false;
+    }
+
+    @Override
+    public int getMaximumVirtualMachineCount() throws CloudException, InternalException {
+        return Capabilities.LIMIT_UNKNOWN;
+    }
+
+    @Override
+    public int getCostFactor(@Nonnull VmState state) throws CloudException, InternalException {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Nonnull
+    @Override
+    public String getProviderTermForVirtualMachine(@Nonnull Locale locale) throws CloudException, InternalException {
+        return "VM";
+    }
+
+    @Nullable
+    @Override
+    public VMScalingCapabilities getVerticalScalingCapabilities() throws CloudException, InternalException {
+        return VMScalingCapabilities.getInstance(false, true, Requirement.NONE, Requirement.REQUIRED);
+    }
+
+    @Nonnull
+    @Override
+    public Requirement identifyImageRequirement(@Nonnull ImageClass cls) throws CloudException, InternalException {
+        return (ImageClass.MACHINE.equals(cls) ? Requirement.REQUIRED : Requirement.OPTIONAL);
+    }
+
+    @Nonnull
+    @Override
+    public Requirement identifyPasswordRequirement(Platform platform) throws CloudException, InternalException {
+        return Requirement.NONE;
+    }
+
+    @Nonnull
+    @Override
+    public Requirement identifyRootVolumeRequirement() throws CloudException, InternalException {
+        return Requirement.REQUIRED;
+    }
+
+    @Nonnull
+    @Override
+    public Requirement identifyShellKeyRequirement(Platform platform) throws CloudException, InternalException {
+        return Requirement.NONE;
+    }
+
+    @Nonnull
+    @Override
+    public Requirement identifyStaticIPRequirement() throws CloudException, InternalException {
+        return Requirement.NONE;
+    }
+
+    @Nonnull
+    @Override
+    public Requirement identifyVlanRequirement() throws CloudException, InternalException {
+        return Requirement.REQUIRED;
+    }
+
+    @Override
+    public boolean isAPITerminationPreventable() throws CloudException, InternalException {
+        return false;
+    }
+
+    @Override
+    public boolean isBasicAnalyticsSupported() throws CloudException, InternalException {
+        return false;
+    }
+
+    @Override
+    public boolean isExtendedAnalyticsSupported() throws CloudException, InternalException {
+        return false;
+    }
+
+    @Override
+    public boolean isUserDataSupported() throws CloudException, InternalException {
+        return false;
+    }
+
+    @Nonnull
+    @Override
+    public Iterable<Architecture> listSupportedArchitectures() throws InternalException, CloudException {
+        ArrayList<Architecture> list = new ArrayList<Architecture>();
+        list.add(Architecture.I32);
+        list.add(Architecture.I64);
+        return list;
+    }
+}
--- src/main/java/org/dasein/cloud/virtustream/compute/VirtualMachines.java
+++ src/main/java/org/dasein/cloud/virtustream/compute/VirtualMachines.java
@@ -22,7 +22,6 @@ package org.dasein.cloud.virtustream.compute;
 import org.apache.log4j.Logger;
 import org.dasein.cloud.CloudException;
 import org.dasein.cloud.InternalException;
-import org.dasein.cloud.ProviderContext;
 import org.dasein.cloud.Requirement;
 import org.dasein.cloud.ResourceStatus;
 import org.dasein.cloud.compute.AbstractVMSupport;
@@ -30,6 +29,7 @@ import org.dasein.cloud.compute.Architecture;
 import org.dasein.cloud.compute.MachineImage;
 import org.dasein.cloud.compute.Platform;
 import org.dasein.cloud.compute.VirtualMachine;
+import org.dasein.cloud.compute.VirtualMachineCapabilities;
 import org.dasein.cloud.compute.VirtualMachineProduct;
 import org.dasein.cloud.compute.VMFilterOptions;
 import org.dasein.cloud.compute.VMLaunchOptions;
@@ -287,6 +287,16 @@ public class VirtualMachines extends AbstractVMSupport {
         finally {
             APITrace.end();
         }
+    }
+
+    private transient volatile VMCapabilities capabilities;
+    @Nonnull
+    @Override
+    public VirtualMachineCapabilities getCapabilities() throws InternalException, CloudException {
+        if( capabilities == null ) {
+            capabilities = new VMCapabilities(provider);
+        }
+        return capabilities;
     }
 
     @Nullable
