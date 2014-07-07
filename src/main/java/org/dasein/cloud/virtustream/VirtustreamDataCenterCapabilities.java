066441642c09797c05b13ff8a130b0c6ee068834
Danielle Mayne <danielle.mayne@enstratus.com>
2014-07-07 09:16:28 +0100

    added datacenter capabilities

---
+++ src/main/java/org/dasein/cloud/virtustream/VirtustreamDataCenterCapabilities.java
@@ -0,0 +1,32 @@
+package org.dasein.cloud.virtustream;
+
+import org.dasein.cloud.AbstractCapabilities;
+import org.dasein.cloud.dc.DataCenterCapabilities;
+
+import javax.annotation.Nonnull;
+import java.util.Locale;
+
+/**
+ * User: daniellemayne
+ * Date: 04/07/2014
+ * Time: 16:30
+ */
+public class VirtustreamDataCenterCapabilities extends AbstractCapabilities<Virtustream> implements DataCenterCapabilities {
+    public VirtustreamDataCenterCapabilities(@Nonnull Virtustream provider) {
+        super(provider);
+    }
+    @Override
+    public String getProviderTermForDataCenter(Locale locale) {
+        return "site";
+    }
+
+    @Override
+    public String getProviderTermForRegion(Locale locale) {
+        return "region";
+    }
+
+    @Override
+    public boolean supportsResourcePools() {
+        return false;
+    }
+}
